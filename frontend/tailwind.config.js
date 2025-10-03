3d14183bb750cc36bc035e1286454b7ff00f846e
Guillermo Giuliana <guillermo@opensupports.com>
2025-10-03 02:53:44 -0300

    feat: aplica marca de Urbano default

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-primary': '#c1292e',
+        'brand-background': '#ffffff',
+        'brand-active': '#c1292e',
+        'brand-header-background': '#e2e1e1',
+        'primary-red': '#c1292e',
+        'red-hover': '#c1292e',
+        'primary-white': '#ffffff',
+        'white-hover': '#f2f2f2',
+      },
+      fontFamily: {
+        'sans': ['Roboto', 'Helvetica', 'Helvetica Neue', 'Nunito Sans'],
+      },
+    },
   },
   variants: {
     extend: {
